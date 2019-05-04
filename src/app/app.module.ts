ec2fe2c59a60f1c6ff06d775e7807e8ec05d6b3d
Agorex <matias_123@live.cl>
2019-05-03 22:08:57 -0400

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateComponent } from './components/template/template.component';
+import { DataComponent } from './components/data/data.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateComponent,
+    DataComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
