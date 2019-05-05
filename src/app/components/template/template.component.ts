357bd3ec40af2cae8b69fbbbaff99cab877475a9
Agorex <matias_123@live.cl>
2019-05-05 14:49:09 -0400

    Radio y Checkbox
    Se agrega radios y checkbox al formulario, se usa ngFor para generar el Radio

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+  .ng-invalid.ng-touched:not(form){
+    border: 1px solid red;
+  }
+  `]
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent {
+
+  usuario: Object = {
+    nombre: null,
+    apellido: null,
+    correo: null,
+    pais: '',
+    sexo: null,
+    acepta: false
+  };
+
+  paises = [{
+    codigo: 'CHI',
+    nombre: 'Chile'
+  },
+  {
+    codigo: 'BRA',
+    nombre: 'Brasil'
+  }
+  ]
+
+  sexos:string[] = ['Hombre', 'Mujer']
 
   constructor() { }
 
+  // tslint:disable-next-line:use-life-cycle-interface
   ngOnInit() {
+  }
+
+  guardar(forma: NgForm) {
+    console.log('ngForm', forma);
+    console.log('valor', forma.value);
+    console.log('Usuario', this.usuario);
   }
 
 }
