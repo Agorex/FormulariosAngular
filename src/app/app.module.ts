fd5253e16701af8a5ef4dcb75c0809c7cc0f174f
Agorex <matias_123@live.cl>
2019-05-05 22:56:57 -0400

    FormData
    Creacion de formulario por data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { DataComponent } from './components/data/data.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { formControlBinding } from '@angular/forms/src/directives/ng_model';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
 
-  constructor() { }
+  forma: FormGroup;
 
-  ngOnInit() {
+  constructor() {
+    this.forma = new FormGroup({
+      'nombre': new FormControl('', [Validators.required, Validators.minLength(4)]),
+      'apellido': new FormControl('', Validators.required),
+      'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+
+    })
   }
+
+  guardarCambios(){
+    console.log(this.forma.value);
+  }
+
+
 
 }
